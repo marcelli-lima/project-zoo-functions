4faf93a91d65b3f9c63d721a7875d844d2161083
marcelli-lima <marcelli_lima@hotmail.com>
2022-07-13 09:45:53 -0300

    função que busca o animal pelo id

--- src/zoo.js
+++ src/zoo.js
@@ -8,12 +8,13 @@ eslint no-unused-vars: [
   }
 ]
 */
-
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids === 'undefined' ? [] : species.filter((element) => ids.includes(element.id));
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
